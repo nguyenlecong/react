102e8cd4e0811806d34b434576595f393967d69e
nguyenlc <nguyen.lc41@gmail.com>
2024-03-31 11:40:40 +0700

    [Hooks] useEffect with DOM events

--- reactjs/tiktok/src/Content.js
+++ reactjs/tiktok/src/Content.js
@@ -12,6 +12,7 @@ import { useEffect, useState } from 'react'
 
 // -----------------------------
 // 1. Callback luôn được gọi sau khi component mounted
+// 2. Cleanup function luôn được gọi trước khi component unmounted
 
 
 const tabs = ['posts', 'comments', 'albums']
@@ -20,16 +21,42 @@ function Content() {
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts')
+    const [showGoToTop, setShowGoToTop] = useState(false)
+    const [width, setWidth] = useState(window.innerWidth)
 
     useEffect(() => {
         document.title = title
+    })
 
+    useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(res => res.json())
         .then(posts => {
             setPosts(posts)
         })
     }, [type])
+
+    useEffect(() => {
+        const handleScroll = () => {setShowGoToTop(window.scrollY >= 200)}
+
+        window.addEventListener('scroll', handleScroll)
+        
+        // Cleanup function
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+
+    useEffect(() => {
+        const handleResize = () => {setWidth(window.innerWidth)}
+        window.addEventListener('resize', handleResize)
+
+        // Cleanup function
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
 
     return (
         <div>
@@ -51,11 +78,27 @@ function Content() {
                     </button>)
             })}
 
+            <h1>
+                {width}
+            </h1>
+
             <ul>
                 {posts.map(post => {
                     return (<li key={post.id}>{post.title  || post.name}</li>)
                 })}
             </ul>
+
+            {showGoToTop && (
+                <button 
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }}
+                >
+                    Go To Top
+                </button>
+            )}
         </div>
     )
 }
