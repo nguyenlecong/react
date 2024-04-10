2dc501284bd4cfac800aa9c426f0663a8e4d41d1
nguyenlecong <nguyen.lc41@gmail.com>
2024-04-10 12:00:12 +0700

    [Hooks] useReducer with Todo App

--- reactjs/tiktok/src/useReducer.js
+++ reactjs/tiktok/src/useReducer.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useReducer, useRef } from 'react'
 
 // useState
 // 1. Init state: 0
@@ -11,34 +11,104 @@ import { useReducer } from 'react'
 // 4. Dispatch
 
 
-// Init state
-const initState = 0
+// // Init state
+// const initState = 0
 
-// Actions
-const UP_ACTION = 'up'
-const DOWN_ACTION = 'down'
+// // Actions
+// const UP_ACTION = 'up'
+// const DOWN_ACTION = 'down'
 
-// Reducer
+// // Reducer
+// const reducer = (state, action) => {
+//   switch(action) {
+//     case UP_ACTION:
+//       return state + 1
+//     case DOWN_ACTION:
+//       return state - 1
+//     default:
+//       throw new Error('Invalid action')
+//   }
+// }
+
+// Todo app
+// 1. Init state
+const initState = {
+  job: '',
+  jobs: []
+}
+
+// 2. Actions
+const SET_JOB = 'set_job'
+const ADD_JOB = 'add_job'
+const DELETE_JOB = 'delete_job'
+
+const setJob = payload => {
+  return {
+    type: SET_JOB,
+    payload
+  }
+}
+
+const addJob = payload => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+
+const deleteJob = payload => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+
+// 3. Reducer
 const reducer = (state, action) => {
-  switch(action) {
-    case UP_ACTION:
-      return state + 1
-    case DOWN_ACTION:
-      return state - 1
+  switch(action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        job: action.payload
+      }
+    case ADD_JOB:
+      return {
+        ...state,
+        jobs: [...state.jobs, action.payload]
+        // job: '',  // Clear input
+      }
+    case DELETE_JOB:
+      const newJobs = [...state.jobs]
+      newJobs.splice(action.payload, 1)
+
+      return {
+          ...state,
+          jobs: newJobs
+        }
     default:
       throw new Error('Invalid action')
   }
 }
 
 // Dispatch
-
 function Content() {
   // const [count, setCount] = useState(0)
-  const [count, dispatch] = useReducer(reducer, initState)
+  // const [count, dispatch] = useReducer(reducer, initState)
+  const [state, dispatch] = useReducer(reducer, initState)
+  const {job, jobs} = state
+
+  const inputRef = useRef()
+
+  const handleSubmit = () => {
+    dispatch(addJob(job))
+    dispatch(setJob(''))  // Clear input
+
+    inputRef.current.focus()
+  }
 
   return (
     <div>
-      <h1>{count}</h1>
+      {/* <h1>{count}</h1>
       <button
         // onClick={() => setCount(count - 1)}
           onClick={() => dispatch(DOWN_ACTION)}
@@ -50,7 +120,27 @@ function Content() {
           onClick={() => dispatch(UP_ACTION)}
       >
         Up
-      </button>
+      </button> */}
+
+      <h1>Todo</h1>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder='Enter todo'
+        onChange={e => {
+          dispatch(setJob(e.target.value))
+        }}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}
+            <span onClick={() => {dispatch(deleteJob(index))}}> 
+              &times;
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
 
   )
